1b1ff237c55a69a3f75f708d66862c29b8267639
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-02-28 18:58:09 +0100

    Create Classes.cs

---
+++ Classes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Collection
+    {
+        Photo[] photos;
+    }
+    class Photo
+    {
+        public int id;
+        public bool horizontal;
+        public List<string> tags;
+    }
+}
