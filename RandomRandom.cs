da2fe15b8cec08a07f5c9dcc93924d2ea6297853
Paweł Flis <47721927+pflis98@users.noreply.github.com>
2019-02-28 20:57:01 +0100

    sdhfas

--- RandomRandom.cs
+++ RandomRandom.cs
@@ -34,5 +34,47 @@ namespace Project
             return res;
 
         }
+        public Photo[] RunProbab(Photo[] input)
+        {
+            Photo[] res = input.Merge().ToArray();
+            Random sh = new Random();
+            //Array.Sort(res, ((prop1, prop2) => sh.Next() % 3 - 1));
+            for (int i = 0; i < 9999; i++)
+            {
+                int a = sh.Next(1, res.Length - 2);
+                int b = sh.Next(1, res.Length - 2);
+                int A1, A2, B1, B2;
+                A1 = Functions.Score(res[a], res[a - 1]) + Functions.Score(res[a], res[a + 1]);
+                A2 = Functions.Score(res[b], res[a - 1]) + Functions.Score(res[b], res[a + 1]);
+                B1 = Functions.Score(res[b], res[b - 1]) + Functions.Score(res[b], res[b + 1]);
+                B2 = Functions.Score(res[a], res[b - 1]) + Functions.Score(res[a], res[b + 1]);
+
+                int tmpFF = sh.Next(100);
+                if (A1 + B1 < A2 + B2)
+                {
+                    if (tmpFF > 3)
+                    {
+                        Photo tmp = res[a];
+                        res[a] = res[b];
+                        res[b] = tmp;
+                    }
+                }
+                else
+                {
+                    if(tmpFF <= 3)
+                    {
+                        Photo tmp = res[a];
+                        res[a] = res[b];
+                        res[b] = tmp;
+                    }
+                }
+               
+            }
+
+            int Score = res.ToList<Photo>().Score();
+            Console.WriteLine("------{0}-------", Score);
+            return res;
+
+        }
     }
 }
