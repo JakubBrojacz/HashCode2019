c1726b75ed2d429f3f1a315ab88f4a9f50ec396a
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-02-28 19:33:42 +0100

    sdsd

--- Main.cs
+++ Main.cs
@@ -9,8 +9,21 @@ namespace Project
     {
         static public void Main()
         {
-            string path = "";
-            Photo[] collection = Input.ReadData(path);
+            Func<Photo[], List<Photo>> Solution = new Func<Photo[], List<Photo>>();
+            int points = 0;
+            Photo[] tab = null;
+            List<Photo> s = null;
+            string[] paths = { "a_example.in", "b_small.in", "c_medium.in", "d_big.in" };
+            foreach (var path in paths)
+            {
+                Console.WriteLine(path);
+                tab = Input.ReadData(path);
+                s = Solution(tab);
+                points = CalcPoints(s);
+                //Output.WriteData("a_example.out", s);
+                Console.WriteLine($"Points: {points}");
+                Console.WriteLine("________");
+            }
             return;
         }
     }
