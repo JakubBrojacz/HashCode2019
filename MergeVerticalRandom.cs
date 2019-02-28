a1750cc747ea54f63f352ad6f4cad93a7ca695be
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-02-28 21:11:12 +0100

    Update MergeVerticalRandom.cs

--- MergeVerticalRandom.cs
+++ MergeVerticalRandom.cs
@@ -43,37 +43,8 @@ namespace Project
         }
         public static List<Photo> MergeRandom(this Photo[] photos)
         {
-            List<Photo> newPhotos = new List<Photo>();
-            List<Photo> verticals = new List<Photo>();
-            for (int i = 0; i < photos.Length; i++)
-            {
-                if (!photos[i].horizontal)
-                {
-                    verticals.Add(photos[i]);
-                }
-                else
-                {
-                    newPhotos.Add(photos[i]);
-                }
-            }
-            Random rand = new Random();
-            while (verticals.Any())
-            {
-                int i1 = rand.Next()%verticals.Count;
-                int i2 = rand.Next() % verticals.Count;
-                if (i1 == i2)
-                    continue;
-                foreach (var tag in verticals[i1].tags)
-                {
-                    verticals[i2].tags.Add(tag);
-                }
-                verticals[i2].id2 = verticals[i1].id;
-                newPhotos.Add(verticals[i2]);
-                Photo dupa = verticals[i2];
-                verticals.Remove(verticals[i1]);
-                verticals.Remove(dupa);
-            }
-            return newPhotos;
+            photos.Shuffle();
+            return Merge(photos);
         }
     }
 }
