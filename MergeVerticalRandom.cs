8a2fdd56c5ffc69558d7d4c5d5b4712e82332426
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-02-28 20:48:09 +0100

    sd

--- MergeVerticalRandom.cs
+++ MergeVerticalRandom.cs
@@ -41,5 +41,38 @@ namespace Project
             return newPhotos;
 
         }
+        public static List<Photo> MergeRandom(this Photo[] photos)
+        {
+            List<Photo> newPhotos = new List<Photo>();
+            List<Photo> verticals = new List<Photo>();
+            for (int i = 0; i < photos.Length; i++)
+            {
+                if (!photos[i].horizontal)
+                {
+                    verticals.Add(photos[i]);
+                }
+                else
+                {
+                    newPhotos.Add(photos[i]);
+                }
+            }
+            Random rand = new Random();
+            while (verticals.Any())
+            {
+                int i1 = rand.Next()%verticals.Count;
+                int i2 = rand.Next() % verticals.Count;
+                if (i1 == i2)
+                    continue;
+                foreach (var tag in verticals[i1].tags)
+                {
+                    verticals[i2].tags.Add(tag);
+                }
+                verticals[i2].id2 = verticals[i1].id;
+                newPhotos.Add(verticals[i2]);
+                verticals.RemoveAt(i1);
+                verticals.RemoveAt(i2);
+            }
+            return newPhotos;
+        }
     }
 }
