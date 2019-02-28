39e699c385403f98317e3c8bf1f6432896112309
Paweł Flis <47721927+pflis98@users.noreply.github.com>
2019-02-28 19:22:28 +0100

    Sm

--- Functions.cs
+++ Functions.cs
@@ -7,15 +7,33 @@ namespace Project
 {
     static class Functions
     {
-        public static int Score(this List<Photo> list)
+        public static int Score(this Photo[] tab)
         {
             int result = 0;
-            var tab = list.ToArray();
             for(int iTab = 0; iTab < tab.Length-1; iTab++)
             {
                 result += tab[iTab].Score(tab[iTab + 1]);
             }
             return result;
         }
+        public static int[,] GenPairs(this Photo[] list)
+        {
+            var tab = new int[list.Length, list.Length];
+            foreach(var mover in list)
+            {
+                foreach(var plox in list)
+                {
+                    if(mover.horizontal == false && plox.horizontal == false)
+                    {
+                        tab[mover.id, plox.id] = -1;
+                        tab[plox.id, mover.id] = -1;
+                        continue;
+                    }
+                    tab[mover.id, plox.id] = tab[plox.id, mover.id] = mover.Score(plox);
+                }
+            }
+            return tab;
+        }
     }
+
 }
