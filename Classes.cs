9f7d6ffa6e73e6c3c8ba8e7703ac219671d649b7
karolk98 <47919588+karolk98@users.noreply.github.com>
2019-02-28 19:45:54 +0100

    sasd

--- Classes.cs
+++ Classes.cs
@@ -15,5 +15,16 @@ namespace Project
         {
             return Math.Min(tags.Intersect(other.tags).Count(), Math.Min(tags.Except(other.tags).Count(), other.tags.Except(tags).Count()));
         }
+
+        public static Photo Concatenate(Photo a,Photo b)
+        {
+            Photo res = new Photo();
+            res.id = -1;
+            res.horizontal = true;
+            res.tags = new HashSet<string>();
+            res.tags.UnionWith(a.tags);
+            res.tags.UnionWith(b.tags);
+            return res;
+        }
     }
 }
