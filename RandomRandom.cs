7827d780ec548f3c5541be1b34f710962f432acd
karolk98 <47919588+karolk98@users.noreply.github.com>
2019-02-28 20:16:36 +0100

    fsdfs

---
+++ RandomRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project
+{
+    class RandomSol
+    {
+        public Photo[] run(Photo[] input)
+        {
+            Photo[] tab = { };//tablica do wprowadzenia z inputap
+            Photo[] res = new Photo[tab.Length];
+            Random sh = new Random();
+            Array.Sort(res, ((prop1, prop2) => sh.Next() % 3 - 1));
+            for(int i=0;i<9999;i++)
+            {
+                int a=sh.Next(res.Length);
+            }
+
+        }
+    }
+}
