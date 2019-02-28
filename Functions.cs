2c920e5427074b11104bc4cb8972e203e2c61d55
Paweł Flis <47721927+pflis98@users.noreply.github.com>
2019-02-28 19:07:23 +0100

    Fun

---
+++ Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HAShCoooDe
+{
+    class Functions
+    {
+        public int Score(List<Photo> list)
+        {
+            int result = 0;
+            var tab = list.ToArray();
+            for(int iTab = 0; iTab < tab.Length-1; iTab++)
+            {
+                result += Score(tab[iTab], tab[iTab + 1]);
+            }
+            return result;
+        }
+    }
+}
